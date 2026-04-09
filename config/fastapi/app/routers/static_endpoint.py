13c3420cf9d8200398ec6f46d1b147231927b375
mog202 <adam.mogielnicki@student.wat.edu.pl>
2026-04-09 09:51:33 +0200

    wip:add dynamic content for users endpoint base on env

--- config/fastapi/app/routers/static_endpoint.py
+++ config/fastapi/app/routers/static_endpoint.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/user")
+@router.get("/user_staic")
 async def get_user():
-    return [{"id":1, "name":"Adam","location":"Szczecin","ports": "123"},
-            {"id": 2, "name": "Rafal", "location": "Turek", "ports": "1234"},
-            {"id":3, "name":"piotr","location":"Garwolin","ports": "12345"},
-            {"id":4, "name":"aleks","location":"Bial","ports": "123456"},
-            {"id":5, "name":"goo","location":"Neer","ports": "1234567"}]
+    return [{"id":1, "name":"Adam","location":"Szczecin","posts": "123"},
+            {"id": 2, "name": "Rafal", "location": "Turek", "posts": "1234"},
+            {"id":3, "name":"piotr","location":"Garwolin","posts": "12345"},
+            {"id":4, "name":"aleks","location":"Bial","posts": "123456"},
+            {"id":5, "name":"goo","location":"Neer","posts": "1234567"}]
 
 
